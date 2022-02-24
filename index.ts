d75c923772959ca1a421f4010f07875fe0afd78f
J Alexander Martinez <Jalexmartinez1013@hotmail.com>
2022-02-24 10:55:41 -0500

    Typescript messing project

---
+++ index.ts
@@ -0,0 +1,21 @@
+interface User {
+    id: string;
+    username: string;
+}
+
+interface Message {
+    id: string;
+    userId: string;
+    text: string;
+    sentAt: Date;
+
+}
+
+
+const createUser = (user: User) => {
+
+}
+
+const sendMessage = (message: Message) => {
+
+}
